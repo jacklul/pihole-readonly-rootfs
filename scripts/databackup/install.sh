721bd7cabe31b0dc6b05f424dfa59e38089310fd
Jack'lul <jacklulcat@gmail.com>
2021-02-06 08:43:41 +0100

    Update scripts, bump year

--- scripts/databackup/install.sh
+++ scripts/databackup/install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-	exec sudo -- "$0" "$@"
-	exit
-fi
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
 set -e
 command -v rsync >/dev/null 2>&1 || { echo "This script requires rsync to run, install it with 'sudo apt install rsync'."; exit 1; }
@@ -18,25 +15,23 @@ SPATH=$(dirname $0)
 REMOTE_URL=https://raw.githubusercontent.com/jacklul/pihole-readonly-rootfs/master/scripts/databackup
 
 if [ -f "$SPATH/databackup.sh" ] && [ -f "$SPATH/databackup.service" ] && [ -f "$SPATH/databackup.timer" ]; then
-	cp -v $SPATH/databackup.sh /usr/local/sbin/databackup && \
-	chmod +x /usr/local/sbin/databackup
+	cp -v $SPATH/databackup.sh /usr/local/sbin/databackup && chmod +x /usr/local/sbin/databackup
 	
 	if [ ! -f "/etc/databackup.conf" ]; then
 		cp -v $SPATH/databackup.conf /etc/databackup.conf
 	fi
 	
-	cp -v $SPATH/databackup.service /etc/systemd/system
-	cp -v $SPATH/databackup.timer /etc/systemd/system
+	cp -v $SPATH/databackup.service /etc/systemd/system && chmod 644 /etc/systemd/system/databackup.service
+	cp -v $SPATH/databackup.timer /etc/systemd/system && chmod 644 /etc/systemd/system/databackup.timer
 elif [ "$REMOTE_URL" != "" ]; then
-	wget -nv -O /usr/local/sbin/databackup "$REMOTE_URL/databackup.sh" && \
-	chmod +x /usr/local/sbin/databackup
+	wget -nv -O /usr/local/sbin/databackup "$REMOTE_URL/databackup.sh" && chmod +x /usr/local/sbin/databackup
 	
 	if [ ! -f "/etc/databackup.conf" ]; then
 		wget -nv -O /etc/databackup.conf "$REMOTE_URL/databackup.conf"
 	fi
 	
-	wget -nv -O /etc/systemd/system/databackup.service "$REMOTE_URL/databackup.service"
-	wget -nv -O /etc/systemd/system/databackup.timer "$REMOTE_URL/databackup.timer"
+	wget -nv -O /etc/systemd/system/databackup.service "$REMOTE_URL/databackup.service" && chmod 644 /etc/systemd/system/databackup.service
+	wget -nv -O /etc/systemd/system/databackup.timer "$REMOTE_URL/databackup.timer" && chmod 644 /etc/systemd/system/databackup.timer
 else
 	exit 1
 fi
--- scripts/datasync/install.sh
+++ scripts/datasync/install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-	exec sudo -- "$0" "$@"
-	exit
-fi
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
 set -e
 command -v rsync >/dev/null 2>&1 || { echo "This script requires rsync to run, install it with 'sudo apt install rsync'."; exit 1; }
@@ -18,15 +15,13 @@ SPATH=$(dirname $0)
 REMOTE_URL=https://raw.githubusercontent.com/jacklul/pihole-readonly-rootfs/master/scripts/datasync/
 
 if [ -f "$SPATH/datasync.sh" ] && [ -f "$SPATH/datasync.service" ] && [ -f "$SPATH/datasync.timer" ]; then
-	cp -v $SPATH/datasync.sh /usr/local/sbin/datasync && \
-	chmod +x /usr/local/sbin/datasync
-	cp -v $SPATH/datasync.service /etc/systemd/system
-	cp -v $SPATH/datasync.timer /etc/systemd/system
+	cp -v $SPATH/datasync.sh /usr/local/sbin/datasync && chmod +x /usr/local/sbin/datasync
+	cp -v $SPATH/datasync.service /etc/systemd/system && chmod 644 /etc/systemd/system/datasync.service
+	cp -v $SPATH/datasync.timer /etc/systemd/system && chmod 644 /etc/systemd/system/datasync.timer
 elif [ "$REMOTE_URL" != "" ]; then
-	wget -nv -O /usr/local/sbin/datasync "$REMOTE_URL/datasync.sh" && \
-	chmod +x /usr/local/sbin/datasync
-	wget -nv -O /etc/systemd/system/datasync.service "$REMOTE_URL/datasync.service"
-	wget -nv -O /etc/systemd/system/datasync.timer "$REMOTE_URL/datasync.timer"
+	wget -nv -O /usr/local/sbin/datasync "$REMOTE_URL/datasync.sh" && chmod +x /usr/local/sbin/datasync
+	wget -nv -O /etc/systemd/system/datasync.service "$REMOTE_URL/datasync.service" && chmod 644 /etc/systemd/system/datasync.service
+	wget -nv -O /etc/systemd/system/datasync.timer "$REMOTE_URL/datasync.timer" && chmod 644 /etc/systemd/system/datasync.timer
 else
 	exit 1
 fi
